c6a330610df3903c86328184fc08909efc77f907
khaled mahmoud <khaled.ma805@gmail.com>
2021-01-17 03:20:47 +0200

    last

--- js/main.js
+++ js/main.js
@@ -3,34 +3,31 @@
 // });
 
 $(function () {
-    // AOS.init({
-    //     once: true,
-    //     disable: "mobile",
-    // });
-    // $(window).on("scroll", function () {
-    //     AOS.refresh();
-    // });
-    if(window.innerWidth > 992){
-      $(".content-wrapper").addClass("active");
-      $(".dash-navbar-left").addClass("active");
-      $("nav").addClass("active");
-      $('nav-torger').addClass("active");
+  // AOS.init({
+  //     once: true,
+  //     disable: "mobile",
+  // });
+  // $(window).on("scroll", function () {
+  //     AOS.refresh();
+  // });
+  if (window.innerWidth > 992) {
+    $(".content-wrapper").addClass("active");
+    $(".dash-navbar-left").addClass("active");
+    $("nav").addClass("active");
+    $("nav-torger").addClass("active");
 
-      $('.nav-torger').click(function(e) {
-        e.preventDefault();
-        $(".content-wrapper").toggleClass("active");
-        $(".dash-navbar-left").toggleClass("active");
-        $("nav").toggleClass("active");
-        $(this).toggleClass("active");
-
-      });
-    }else {
-      $('.nav-torger , .close-sm').click(function(e) {
-        e.preventDefault();
-        $(".dash-navbar-left").toggleClass("active");
-        $('.nav-torger').toggleClass("active");
-      });
-
-    }
-
+    $(".nav-torger").click(function (e) {
+      e.preventDefault();
+      $(".content-wrapper").toggleClass("active");
+      $(".dash-navbar-left").toggleClass("active");
+      $("nav").toggleClass("active");
+      $(this).toggleClass("active");
+    });
+  } else {
+    $(".nav-torger , .close-sm").click(function (e) {
+      e.preventDefault();
+      $(".dash-navbar-left").toggleClass("active");
+      $(".nav-torger").toggleClass("active");
+    });
+  }
 });
